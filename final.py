3fe036aa7aba3879be103af558cbadcb527f17c5
Chris Musialek <musialek@cs.umd.edu>
2014-12-01 17:58:51 -0500

    Code to Create VW files in proper format, seems to work

--- final.py
+++ final.py
@@ -3,22 +3,34 @@ import numpy
 import pickle
 import os
 from util import *
+import re
 
 PKL_TRAIN = 'data/train.pkl'
 PKL_DEV = 'data/dev.pkl'
 TRAIN = 'data/train.csv'
-
+VW_INPUT_TRAIN = 'vw/qa_vw.tr'
+VW_INPUT_DEV = 'vw/qa_vw.de'
+VW_MODEL = 'vw/qa_vw.model'
+vw_bin = '/usr/local/bin/vw'
 
 def main():
-    answer_map = [] #Index is the map
+    #answer_map = [] #Index is the map
     if (not os.path.isfile(PKL_TRAIN)) | (not os.path.isfile(PKL_DEV)):
         train = load_training_data(TRAIN)
         generate_train_dev(train)
 
     train = pickle.load(open(PKL_TRAIN, "rb"))
     dev = pickle.load(open(PKL_DEV, "rb"))
-    answer_map = create_answer_map(train)
+    #answer_map = create_answer_map(train)
 
+    print 'Generating Classification Data'
+    generateVWData(dev, VW_INPUT_DEV)
+    generateVWData(train, VW_INPUT_TRAIN)
+    
+    trainVW(VW_INPUT_DEV, VW_MODEL, True)
+    
+    train_pred = testVW(VW_INPUT_DEV, VW_MODEL, True)
+    
     print len(train)
     print len(dev)
 
@@ -90,12 +102,138 @@ def set_dev_data(data):
     dev = selected_data[split + 1:]
     return train, dev
 
+def answerFeatures(item):
+    array_of_answers = []
+    correct_answer = item['answer']
+    for k,v in item['wiki_score'].items():
+        feats = Counter()
+        feats['a_' + v] = 1
+        feats['wiki_prob'] = k
+        
+        isCorrect = 1 #False
+        if v == correct_answer:
+            isCorrect = 0
+        
+        #append new answer to array_of_answers
+        new_answer = (isCorrect, feats, {})
+        array_of_answers.append(new_answer)
+        
+    for k,v in item['quanta'].items():
+        feats = Counter()
+        feats['a_' + v] = 1
+        feats['quanta_prob'] = k
+
+        isCorrect = 1 #False
+        if v == correct_answer:
+            isCorrect = 0
+        
+        #append new answer to array_of_answers
+        new_answer = (isCorrect, feats, {})
+        array_of_answers.append(new_answer)
+    
+    return array_of_answers
+
+
 def questionFeatures(item):
-    category = item['category']
+    category = 'cat_' + item['category']
     feats = Counter()
     feats[category] = 1
 
     return feats
+
+
+def generateVWData(data, outputFilename=None):
+    h = open(outputFilename, 'w')
+    for item in data:
+        q_feats = questionFeatures(item)
+        a_feats = answerFeatures(item)
+        example = (q_feats, a_feats)
+        writeVWExample(h, example, {})
+    h.close()
+
+
+# write a vw-style example to file
+def writeVWExample(h, example, featureSetTracker=None):
+    def sanitizeFeature(f):
+        return re.sub(':', '_COLON_',
+                      re.sub('\|', '_PIPE_',
+                             re.sub('[\s]', '_', f)))
+
+    def printFeatureSet(namespace, fdict):
+        h.write(' |')
+        h.write(namespace)
+        for f, v in fdict.iteritems():
+            h.write(' ')
+            if abs(v) > 1e-6:
+                ff = sanitizeFeature(f)
+                h.write(ff)
+                if abs(v - 1) > 1e-6:
+                    h.write(':')
+                    h.write(str(v))
+                if featureSetTracker is None:
+                    if not featureSetTracker.has_key(namespace): featureSetTracker[namespace] = {}
+                    featureSetTracker[namespace][ff] = f
+
+    (src, trans) = example
+    if len(src) > 0:
+        h.write('shared')
+        printFeatureSet('s', src)
+        h.write('\n')
+    for i in range(len(trans)):
+        (cost, tgt, pair) = trans[i]
+        h.write(str(i + 1))
+        h.write(':')
+        h.write(str(cost))
+        printFeatureSet('t', tgt)
+        printFeatureSet('p', pair)
+        h.write('\n')
+    h.write('\n')
+
+
+def trainVW(dataFilename, modelFilename, quietVW=False):
+    cmd = vw_bin + ' -k -c -b 25 --holdout_off --passes 10 -q st --power_t 0.5 --csoaa_ldf m -d ' + dataFilename + ' -f ' + modelFilename
+    if quietVW: cmd += ' --quiet'
+    print 'executing: ', cmd
+    p = os.system(cmd)
+    if p != 0:
+        raise Exception('execution of vw failed!  return value=' + str(p))
+
+
+def testVW(dataFilename, modelFilename, quietVW=False):
+    cmd = vw_bin + ' -t -q st -d ' + dataFilename + ' -i ' + modelFilename + ' -r ' + dataFilename + '.rawpredictions'
+    if quietVW: cmd += ' --quiet'
+    print 'executing: ', cmd
+    p = os.system(cmd)
+    if p != 0:
+        raise Exception('execution of vw failed!  return value=' + str(p))
+
+    h = open(dataFilename + '.rawpredictions')
+    predictions = []
+
+    this = []
+    thisBestId = -1
+    thisBestVal = 0
+    for l in h.readlines():
+        l = l.strip()
+        res = l.split(':')
+        if len(l) == 0:
+            predictions.append((thisBestId - 1, predictions))
+            this = []
+            thisBestId = -1
+            thisBestVal = 0
+        elif len(res) == 2:
+            class_id = int(res[0])
+            class_val = float(res[1])
+            if thisBestId < 0 or class_val < thisBestVal:
+                thisBestId = class_id
+                thisBestVal = class_val
+            this.append((class_id, class_val))
+        else:
+            raise Exception('error on vw output, got line "' + l + '"')
+    h.close()
+
+    return predictions
+
 
 if __name__ == "__main__":
     main()
