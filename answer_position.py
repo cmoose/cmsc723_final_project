2e8003e4de656e4b824d1e950733209a1231c738
Chris Musialek <musialek@cs.umd.edu>
2014-12-09 14:37:41 -0500

    correctly choosing

--- answer_position.py
+++ answer_position.py
@@ -4,22 +4,41 @@ import random
 import final
 
 def main():
-    data = final.full_data(final.load_training_data('data/train.csv'), True)
+    csvreader = csv.reader(open('data/train.csv'))
     fhw = open('data/ans_positions.csv', 'wb')
+    data = []
+    for line in csvreader:
+        q_id = line[0]
+        quanta_answers_str = line[2]
+        quanta_answers = []
+        correct_answer = line[3]
+        wiki_answers_str = line[5]
+        wiki_answers = []
+        for qa in quanta_answers_str.split(','):
+            quanta = qa.split(':')[0]
+            quanta_answers.append(quanta)
+        for wa in wiki_answers_str.split(','):
+            wiki = wa.split(':')[0]
+            wiki_answers.append(wiki)
+        data.append([q_id,correct_answer,quanta_answers,wiki_answers])
     for question in data:
-        q_id = question['id']
-        ans = question['answer']
+        q_id = question[0]
+        ans = question[1]
+        quanta_answers = question[2]
+        wiki_answers = question[3]
         i = 1
         wiki_pos = -1
-        for k,v in question['wiki'].items():
-            if ans == v:
+        wiki_array = []
+        for wa in wiki_answers:
+            if ans == wa:
                 wiki_pos = i
                 break
             i+=1
         j = 1
         quanta_pos = -1
-        for k,v in question['quanta'].items():
-            if ans == v:
+        quanta_array = []
+        for qa in quanta_answers:
+            if ans == qa:
                 quanta_pos = j
             j+=1
         fhw.write('%s,%d,%d\n' % (q_id,wiki_pos,quanta_pos))
